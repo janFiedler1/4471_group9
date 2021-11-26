978c4421a30aaaf0268b584b6c9d6adcee2ab159
janFiedler1 <50682895+janFiedler1@users.noreply.github.com>
2021-11-25 20:11:18 -0500

    Add files via upload

---
+++ main-tabs.js
@@ -0,0 +1,9 @@
+document.getElementById("feed-tab").addEventListener("click", aa);
+document.getElementById("sub-tab").addEventListener("click", bb);
+
+function aa() {
+  document.getElementById("tbab").innerHTML = "option 1";
+}
+function bb() {
+  document.getElementById("tbab").innerHTML = "option 2";
+}
