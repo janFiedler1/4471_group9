cacf2edc556d3d0bc51b3e8321f099f160567b75
janFiedler1 <50682895+janFiedler1@users.noreply.github.com>
2021-11-25 23:06:35 -0500

    buttons work differently now

--- main-tabs.js
+++ main-tabs.js
@@ -1,9 +1,17 @@
-document.getElementById("feed-tab").addEventListener("click", aa);
-document.getElementById("sub-tab").addEventListener("click", bb);
+document.getElementById("feed-tab").addEventListener("click", switchTab(event,"feed");
+document.getElementById("sub-tab").addEventListener("click", switchTab(event,"subscribe"));
 
-function aa() {
-  document.getElementById("tbab").innerHTML = "option 1";
-}
-function bb() {
-  document.getElementById("tbab").innerHTML = "option 2";
+
+function switchTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
